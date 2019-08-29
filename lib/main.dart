ceb42c220f6550ce66e078b8a9a2f46e913af18d
unknown <aditya_hermansyahputra@yahoo.com>
2019-08-30 00:09:28 +0700

    add grid

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,45 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
-  List<Container> listResep = List();
+  List<Container> listRecipes = List();
+
+  var recipe = [
+    {"name": "Daging Sapi", "image": "daging_sapi.jpg"},
+    {"name": "Ikan Tauco", "image": "ikan_tauco.jpg"},
+    {"name": "Kol Kukus", "image": "kol_kukus.jpg"},
+    {"name": "Nasi Goreng", "image": "nasi_goreng.jpg"},
+    {"name": "Orak Arik", "image": "orak_arik.jpg"},
+    {"name": "Nasi Goreng Rempah", "image": "nasi_goreng_rempah.jpg"},
+    {"name": "Sayur Lodeh", "image": "sayur_lodeh.jpg"},
+    {"name": "Tumis Jagung", "image": "tumis_jagung.jpg"},
+    {"name": "Udang Saos", "image": "udang_saos.jpg"},
+    {"name": "Daging Sapi Semur", "image": "daging_sapi_semur.jpg"}
+  ];
+
+  _doList() async {
+    for (var i = 0; i < recipe.length; i++) {
+      final recipes = recipe[i];
+      final String image = recipes["image"];
+      listRecipes.add(Container(
+        padding: EdgeInsets.all(8),
+        child: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset("image/$image", fit: BoxFit.cover),
+              Padding(padding: EdgeInsets.all(8)),
+              Text(recipes["name"], style: TextStyle(fontSize: 14))
+            ],
+          ),
+        ),
+      ));
+    }
+  }
+
+  @override
+  void initState() {
+    _doList();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +64,7 @@ class _MainState extends State<Main> {
       ),
       body: GridView.count(
         crossAxisCount: 2,
-        children: listResep,
+        children: listRecipes,
       ),
     );
   }
