61732f30e584a44ee81240d8c3935ba53187430a
unknown <aditya_hermansyahputra@yahoo.com>
2019-08-30 06:39:37 +0700

    fixing

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Meals Catalogue",
+    debugShowCheckedModeBanner: false,
     home: Main(),
   ));
 }
@@ -37,7 +38,17 @@ class _MainState extends State<Main> {
         child: Card(
           child: Column(
             children: <Widget>[
-              Image.asset("image/$image", fit: BoxFit.cover),
+              Hero(
+                tag: recipes["name"],
+                child: Material(
+                  child: InkWell(
+                    child: Image.asset("image/$image", fit: BoxFit.cover),
+                    onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                        builder: (BuildContext context) => Detail(
+                            name: recipes["name"], image: recipes["image"]))),
+                  ),
+                ),
+              ),
               Padding(padding: EdgeInsets.all(8)),
               Text(recipes["name"], style: TextStyle(fontSize: 14))
             ],
@@ -57,14 +68,68 @@ class _MainState extends State<Main> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.orange,
         title: Text(
           "Meals Catalogue",
-          style: TextStyle(color: Colors.orange),
         ),
       ),
       body: GridView.count(
         crossAxisCount: 2,
         children: listRecipes,
+      ),
+    );
+  }
+}
+
+class Detail extends StatelessWidget {
+  final String name, image;
+
+  Detail({this.name, this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text("Resep $name"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 240,
+            child: Hero(
+                tag: name,
+                child: Material(
+                  child: InkWell(
+                      child: Image.asset("image/$image", fit: BoxFit.cover)),
+                )),
+          ),
+          new Title(name: name)
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  Title({this.name});
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(name,
+              style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange)),
+          Text("$name@gmail.com", style: TextStyle(fontSize: 12))
+        ],
       ),
     );
   }
